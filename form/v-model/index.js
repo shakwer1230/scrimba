fc933911ac9d825f13ed40b270395c5f28b5982d
shakwer <shakwer@live.cn>
2019-03-31 20:35:53 +0800

    表单

---
+++ form/v-model/index.js
@@ -0,0 +1,14 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'hello vue',
+        checked:true,
+        checkedNames:[],
+        picked:'',
+        selected:'',
+        selecteB:[],
+        toggle:true,
+        msg:''
+    }    
+});
+
